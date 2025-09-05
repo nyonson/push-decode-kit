1ef506ba6d06dc500b5af3d1a87430fdee36b014
Nick Johnson <nick@yonson.dev>
2025-09-05 11:17:28 -0700

    feat!: drop utf8 decoder

--- src/decoders/mod.rs
+++ src/decoders/mod.rs
@@ -7,9 +7,6 @@ mod int;
 #[cfg(feature = "alloc")]
 mod byte_vec;
 
-#[cfg(feature = "alloc")]
-mod utf8_string;
-
 pub use byte_array::ByteArrayDecoder;
 pub use u8_decoder::U8Decoder;
 pub use int::*;
@@ -17,8 +14,4 @@ pub use int::*;
 #[cfg(feature = "alloc")]
 pub use byte_vec::ByteVecDecoder;
 
-#[cfg(feature = "alloc")]
-pub use utf8_string::Utf8StringDecoder;
 
-#[cfg(feature = "alloc")]
-pub use utf8_string::Error as Utf8StringError;
